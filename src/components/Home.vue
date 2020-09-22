21edee620e43cd62257cd0cc56ba28f9bd7978cc
Danny Lai <dannyphlai@gmail.com>
2020-09-22 19:24:12 +1000

    Removed HelloWorld.vue component and added Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h4>{{ msg }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Lorem ipsum'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
