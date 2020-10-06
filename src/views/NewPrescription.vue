20d1c361e4e60c96d7fb413b307cc2221e8bc361
Danny Lai <dannyphlai@gmail.com>
2020-10-07 10:59:05 +1100

    New prescription form validation

--- src/views/NewPrescription.vue
+++ src/views/NewPrescription.vue
@@ -3,7 +3,14 @@
 
   <h3>Issue new prescription</h3>
 
-  <form @submit.prevent="issueScript">
+  <div v-if="errors.length" class="errors">
+    Please resolve the following error(s):<br><br>
+    <div v-for="error in errors">{{error}}</div>
+    <br>
+  </div>
+
+
+  <form @submit.prevent="checkForm">
     <label>Patient Name:</label>
     <input type="text" v-model="patientName">
     <label>Patient Medicare Number:</label>
@@ -36,18 +43,38 @@ export default {
 
   data(){
     return {
-      // currentUser: {},
       patientName: '',
       patientMedicareNumber: '',
       patientAddress: '',
       itemName: '',
       dosageInstructions: '',
       quantity: '',
-      issuedByDoctor: ''
+      issuedByDoctor: '',
+      errors: []
     }
   },
 
   methods: {
+
+    checkForm(){
+      if(!this.patientName) this.errors.push("Patient name required.");
+      if(!this.patientMedicareNumber) this.errors.push("Patient medicare number required.");
+      if(!this.patientAddress) this.errors.push("Patient address required.");
+      if(!this.itemName) this.errors.push("Item name required.");
+      if(!this.dosageInstructions) this.errors.push("Dosage instructions required.");
+      if(!this.quantity) this.errors.push("Quantity required.");
+
+      if(this.patientName
+        && this.patientMedicareNumber
+        && this.patientAddress
+        && this.itemName
+        && this.dosageInstructions
+        && this.quantity
+      ){
+        this.issueScript();
+      }
+    },
+
     issueScript(){
       axios.post(PRESCRIPTIONS_URL, {
         patientName: this.patientName,
@@ -89,6 +116,7 @@ label {
   text-align: right;
   padding-right: 10px;
 }
+
 button {
   background-color: rgb(0, 50, 66);
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -104,6 +132,13 @@ button {
 button:hover {
   letter-spacing: 1px;
   background-color: rgb(0, 132, 175);
+}
+
+.errors {
+  display: flex;
+  flex-direction: column;
+  color: red;
+  font-weight: bold;
 }
 
 </style>
