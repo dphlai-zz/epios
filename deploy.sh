3784b3520b2b06ccfc4002acc53caf737e5b3b99
Danny Lai <dannyphlai@gmail.com>
2020-10-07 11:06:08 +1100

    Deploy config

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,14 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
+atom --wait index.html
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dphlai/epios.git master:gh-pages
+# git push -f git@github.com:dphlai/epios.git master:gh-pages
+git push -f https://github.com/dphlai/epios.git master:gh-pages
+# git push -u master:gh-pages
 
 cd -
